51b6eaad1c026d19c433ee37ce9c7b044d446584
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-18 23:53:57 +0530

    props system

---
+++ src/ProfileCard.js
@@ -0,0 +1,9 @@
+function ProfileCard(props){
+    return <div>
+        <h2>Profile Card</h2>
+        <h5>Title is {props.title}</h5>
+        <h5>Social handle is {props.handle}</h5>
+        </div>
+}
+
+export default ProfileCard;
