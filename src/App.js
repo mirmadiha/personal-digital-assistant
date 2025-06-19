1abb895cdab351930de48046a31011650e4c9c88
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-19 23:57:51 +0530

    handling image accessibility

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App(){
     return(
         <div>
             <div><h1>Personal Digital Assistance</h1></div>
 
-            <ProfileCard title="Alexa" handle="@alexa2025" />
-            <ProfileCard title="Cortana" handle="@cortana2025"/>
-            <ProfileCard title="Siri" handle="@siri2025"/>
+            <ProfileCard tatle="Alexa" handle="@alexa2025" image={AlexaImage}/>
+            <ProfileCard title="Cortana" handle="@cortana2025" image={CortanaImage}/>
+            <ProfileCard title="Siri" handle="@siri2025" image={SiriImage}/>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
-function ProfileCard({title,handle}){
+function ProfileCard({title,handle,image}){
     return <div>
         <h2>Profile Card</h2>
+        <img src={image}/>
         <h5>Title is {title}</h5>
         <h5>Social handle is {handle}</h5>
         </div>
