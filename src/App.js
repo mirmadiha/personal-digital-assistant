c8b054f7e46db8f714970f351e891652d7d30d6a
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-21 23:49:21 +0530

    final styling

--- src/App.js
+++ src/App.js
@@ -7,11 +7,47 @@ import SiriImage from './images/siri.png'
 function App(){
     return(
         <div>
-            <div><h1>Personal Digital Assistance</h1></div>
+        <section class="hero is-primary">
+        <div class="hero-body">
+            <p class="title">Personal Digital Assistance</p>
+        </div>
+        </section>
+            
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Alexa" 
+                            handle="@alexa2025" 
+                            image={AlexaImage}
+                            description="Amazon’s virtual assistant, great for smart home control and voice shopping."
+                            />
+                        </div>
 
-            <ProfileCard tatle="Alexa" handle="@alexa2025" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@cortana2025" image={CortanaImage}/>
-            <ProfileCard title="Siri" handle="@siri2025" image={SiriImage}/>
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Cortana" 
+                            handle="@cortana2025" 
+                            image={CortanaImage}
+                            description="Microsoft’s assistant, focused on productivity and Windows integration."
+                            />
+
+                        </div>
+
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Siri" 
+                            handle="@siri2025" 
+                            image={SiriImage}
+                            description="Apple’s voice assistant, built for iPhone and Apple device users."
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({title,handle,image}){
+function ProfileCard({title,handle,image,description}){
     return(
         <div class="card">
         <div class="card-image">
@@ -14,6 +14,7 @@ function ProfileCard({title,handle,image}){
             <p className="title is-4">{title}</p>
             <p className="subtitle is-6">{handle}</p>
             </div>
+            <div className="content">{description}</div>
         </div>
     </div>
     );
